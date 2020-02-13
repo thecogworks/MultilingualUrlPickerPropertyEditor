76e81b54487631c0e6a921520e7c20d1bf6a172e
gmargol <greg.margol@wearecogworks.com>
2020-02-13 16:25:11 +0100

    initial commit

---
+++ Source/MultilingualUrlPickerPropertyEditor/App_Plugins/MultilingualUrlPickerPropertyEditor/js/multilingualurlpicker.picker.controller.js
@@ -0,0 +1,226 @@
+﻿angular.module('umbraco').controller('MultilingualUrlPickerController.Picker', function (
+    $scope,
+    eventsService,
+    entityResource,
+    mediaResource,
+    udiParser,
+    localizationService,
+    languageResource,
+    $cookies,
+    $location) {
+
+    var vm = this;
+    var dialogOptions = $scope.model;
+    vm.submit = submit;
+    vm.close = close;
+    vm.toggleOpenInNewWindow = toggleOpenInNewWindow;
+    vm.labels = {};
+    vm.languages = [];
+    vm.selectedLanguage = {};
+    vm.languageSelectorIsOpen = false;
+    vm.showLanguageSelector = $scope.model.showLanguageSelector;
+    vm.toggleLanguageSelector = toggleLanguageSelector;
+    vm.selectLanguage = selectLanguage;
+    vm.showTree = false;
+
+    console.log($scope.model);
+
+    var defaultCulture = $cookies.get('UMB_MCULTURE');
+
+    if (!defaultCulture) {
+        defaultCulture = $location.search().mculture;
+    }
+
+
+    // load languages
+    languageResource.getAll().then(function (languages) {
+        vm.languages = languages;
+        // set the default language
+        vm.languages.forEach(function (language) {
+            console.log(language.culture, defaultCulture);
+            if (language.culture === defaultCulture) {
+                vm.selectedLanguage = language;
+                vm.languageSelectorIsOpen = false;
+            }
+        });
+
+        initPicker();
+        
+        $scope.customTreeParams = $scope.customTreeParams.length > 0
+            ? $scope.customTreeParams + '&culture=' + vm.selectedLanguage.culture
+            : 'culture=' + vm.selectedLanguage.culture;
+        vm.showTree = true;
+    });
+
+    localizationService.localizeMany(['defaultdialogs_openInNewWindow']).then(function (data) {
+        vm.labels.openInNewWindow = data[0];
+    });
+
+    if (!$scope.model.title) {
+        localizationService.localize('defaultdialogs_selectLink').then(function (value) {
+            $scope.model.title = value;
+        });
+    }
+
+    
+    $scope.customTreeParams = '';
+    $scope.dialogTreeApi = {};
+    $scope.model.target = {};
+    $scope.showTarget = $scope.model.hideTarget !== true;
+
+    // this ensures that we only sync the tree once and only when it's ready
+    var oneTimeTreeSync = {
+        executed: false,
+        treeReady: false,
+        sync: function sync() {
+            // don't run this if:
+            // - it was already run once
+            // - the tree isn't ready yet
+            // - the model path hasn't been loaded yet
+            if (this.executed || !this.treeReady || !($scope.model.target && $scope.model.target.path)) {
+                return;
+            }
+            this.executed = true;
+            // sync the tree to the model path
+            $scope.dialogTreeApi.syncTree({
+                path: $scope.model.target.path,
+                tree: 'content'
+            });
+        }
+    };
+
+    function initPicker() {
+        if (dialogOptions.currentTarget) {
+            // clone the current target so we don't accidentally update the caller's model while manipulating $scope.model.target
+            $scope.model.target = angular.copy(dialogOptions.currentTarget);
+            // if we have a node ID, we fetch the current node to build the form data
+            if ($scope.model.target.culture && $scope.model.target.culture.length) {
+                console.log('languages ', vm.languages);
+                vm.languages.forEach(function (language) {
+                    console.log('lang ', language.culture, $scope.model.target.culture);
+                    if (language.culture === $scope.model.target.culture) {
+                        vm.selectedLanguage = language;
+                    }
+                });
+            }
+
+            if ($scope.model.target.id || $scope.model.target.udi) {
+                // will be either a udi or an int
+                var id = $scope.model.target.udi ? $scope.model.target.udi : $scope.model.target.id;
+
+                entityResource.getPath(id, 'Document').then(function (path) {
+                    $scope.model.target.path = path;
+                    oneTimeTreeSync.sync();
+                });
+                entityResource.getUrlAndAnchors(id).then(function (resp) {
+                    $scope.anchorValues = resp.anchorValues;
+                    $scope.model.target.url = resp.url;
+                });
+
+                entityResource.getUrl($scope.model.target.id, 'Document', $scope.model.target.culture).then(function (resp) {
+                    console.log(resp);
+                    $scope.model.target.url = resp;
+                });
+
+            } else if ($scope.model.target.url && $scope.model.target.url.length) {
+                // a url but no id/udi indicates an external link - trim the url to remove the anchor/qs
+                // only do the substring if there's a # or a ?
+                var indexOfAnchor = $scope.model.target.url.search(/(#|\?)/);
+                if (indexOfAnchor > -1) {
+                    // populate the anchor
+                    $scope.model.target.anchor = $scope.model.target.url.substring(indexOfAnchor);
+                    // then rewrite the model and populate the link
+                    $scope.model.target.url = $scope.model.target.url.substring(0, indexOfAnchor);
+                }
+            }
+            // need to translate the link target ("_blank" or "") into a boolean value for umb-checkbox
+            vm.openInNewWindow = $scope.model.target.target === '_blank';
+        } else if (dialogOptions.anchors) {
+            $scope.anchorValues = dialogOptions.anchors;
+        }
+    }
+
+    function selectLanguage(language) {
+        vm.selectedLanguage = language;
+        // close the language selector
+        vm.languageSelectorIsOpen = false;
+        $scope.dialogTreeApi.load({
+            section: 'content',
+            customTreeParams: 'culture=' + vm.selectedLanguage.culture
+        });
+    }
+
+    function toggleLanguageSelector() {
+        vm.languageSelectorIsOpen = !vm.languageSelectorIsOpen;
+    }
+
+    function treeLoadedHandler(args) {
+        oneTimeTreeSync.treeReady = true;
+        oneTimeTreeSync.sync();
+    }
+    function nodeSelectHandler(args) {
+        if (args && args.event) {
+            args.event.preventDefault();
+            args.event.stopPropagation();
+        }
+        eventsService.emit('dialogs.linkPicker.select', args);
+        if ($scope.currentNode) {
+            //un-select if there's a current one selected
+            $scope.currentNode.selected = false;
+        }
+
+        $scope.currentNode = args.node;
+        $scope.currentNode.selected = true;
+        $scope.model.target.id = args.node.id;
+        $scope.model.target.udi = args.node.udi;
+        $scope.model.target.name = args.node.name;
+        $scope.model.target.culture = args.node.metaData.culture;
+
+        if (args.node.id < 0) {
+            $scope.model.target.url = '/';
+        } else {
+            entityResource.getUrlAndAnchors(args.node.id).then(function (resp) {
+                $scope.anchorValues = resp.anchorValues;
+            });
+            entityResource.getUrl(args.node.id, 'Document', vm.selectedLanguage.culture).then(function(resp) {
+                $scope.model.target.url = resp;
+            });
+        }
+    }
+    function nodeExpandedHandler(args) {
+        // open mini list view for list views
+        if (args.node.metaData.isContainer) {
+            openMiniListView(args.node);
+        }
+    }
+
+    $scope.onTreeInit = function () {
+        $scope.dialogTreeApi.callbacks.treeLoaded(treeLoadedHandler);
+        $scope.dialogTreeApi.callbacks.treeNodeSelect(nodeSelectHandler);
+        $scope.dialogTreeApi.callbacks.treeNodeExpanded(nodeExpandedHandler);
+    };
+    // Mini list view
+    $scope.selectListViewNode = function (node) {
+        node.selected = node.selected === true ? false : true;
+        nodeSelectHandler({ node: node });
+    };
+    $scope.closeMiniListView = function () {
+        $scope.miniListView = undefined;
+    };
+    function openMiniListView(node) {
+        $scope.miniListView = node;
+    }
+    function toggleOpenInNewWindow(model, value) {
+        $scope.model.target.target = model ? '_blank' : '';
+    }
+    function close() {
+        if ($scope.model && $scope.model.close) {
+            $scope.model.close();
+        }
+    }
+    function submit() {
+        if ($scope.model && $scope.model.submit) {
+            $scope.model.submit($scope.model);
+        }
+    }
+});
---
+++ Source/MultilingualUrlPickerPropertyEditor/App_Plugins/MultilingualUrlPickerPropertyEditor/js/multilingualurlpicker.propertyEditor.controller.js
@@ -0,0 +1,156 @@
+﻿angular.module("umbraco").controller("MultilingualUrlPickerController.PropertyEditor",
+    function ( $scope,
+    angularHelper,
+    localizationService,
+    entityResource,
+    iconHelper,
+    editorService) {
+
+
+    var vm = this;
+    vm.openLinkPicker = openLinkPicker;
+
+    $scope.renderModel = [];
+
+    if ($scope.preview) {
+        return;
+    }
+
+    if (!Array.isArray($scope.model.value)) {
+        $scope.model.value = [];
+    }
+
+    var currentForm = angularHelper.getCurrentForm($scope);
+    $scope.sortableOptions = {
+        distance: 10,
+        tolerance: 'pointer',
+        scroll: true,
+        zIndex: 6000,
+        update: function update() {
+            currentForm.$setDirty();
+        }
+    };
+
+    $scope.model.value.forEach(function (link) {
+        link.icon = iconHelper.convertFromLegacyIcon(link.icon);
+        $scope.renderModel.push(link);
+    });
+
+    $scope.$on('formSubmitting', function () {
+        $scope.model.value = $scope.renderModel;
+    });
+
+    $scope.remove = function ($index) {
+        $scope.renderModel.splice($index, 1);
+        currentForm.$setDirty();
+    };
+
+    $scope.$watch(
+        function () {
+            return $scope.renderModel.length;
+        },
+        function () {
+            if ($scope.model.config && $scope.model.config.minNumber) {
+                $scope.multiUrlPickerForm.minCount.$setValidity(
+                    "minCount",
+                    +$scope.model.config.minNumber <= $scope.renderModel.length
+                );
+            }
+            if ($scope.model.config && $scope.model.config.maxNumber) {
+                $scope.multiUrlPickerForm.maxCount.$setValidity(
+                    "maxCount",
+                    +$scope.model.config.maxNumber >= $scope.renderModel.length
+                );
+            }
+            $scope.sortableOptions.disabled = $scope.renderModel.length === 1;
+        }
+        );
+
+    function init() {
+        localizationService.localizeMany(['general_recycleBin']).then(function (data) {
+            vm.labels.general_recycleBin = data[0];
+        });
+        // if the property is mandatory, set the minCount config to 1 (unless of course it is set to something already),
+        // that way the minCount/maxCount validation handles the mandatory as well
+        if ($scope.model.validation && $scope.model.validation.mandatory && !$scope.model.config.minNumber) {
+            $scope.model.config.minNumber = 1;
+        }
+    }
+
+
+    function openLinkPicker(link, $index) {
+        var target = link ? {
+            id: link.id,
+            name: link.name,
+            anchor: link.queryString,
+            udi: link.udi,
+            url: link.url,
+            target: link.target,
+            culture: link.culture
+        } : null;
+
+        var linkpicker = {
+            currentTarget: target,
+            view: "/App_Plugins/MultilingualUrlPickerPropertyEditor/multilingualurlpicker.picker.html",
+            title: "Multilingual Url Picker",
+            size: "small",
+            submit: function (model) {
+                if (model.target.url || model.target.anchor) {
+                    // if an anchor exists, check that it is appropriately prefixed
+                    if (model.target.anchor &&
+                        model.target.anchor[0] !== '?' &&
+                        model.target.anchor[0] !== '#') {
+                        model.target.anchor = (model.target.anchor.indexOf('=') === -1 ? '#' : '?') +
+                            model.target.anchor;
+                    }
+                    if (link) {
+                        link.id = model.target.id;
+                        link.udi = model.target.udi;
+                        link.name = model.target.name || model.target.url || model.target.anchor;
+                        link.queryString = model.target.anchor;
+                        link.target = model.target.target;
+                        link.url = model.target.url;
+                        link.culture = model.target.culture;
+                    } else {
+                        link = {
+                            id: model.target.id,
+                            name: model.target.name || model.target.url || model.target.anchor,
+                            queryString: model.target.anchor,
+                            target: model.target.target,
+                            udi: model.target.udi,
+                            url: model.target.url,
+                            culture: model.target.culture
+                        };
+                        $scope.renderModel.push(link);
+                    }
+                    if (link.udi) {
+                        var entityType = 'Document';
+                        entityResource.getById(link.udi, entityType).then(function (data) {
+                            link.icon = iconHelper.convertFromLegacyIcon(data.icon);
+                            link.published =
+                                data.metaData &&
+                                    data.metaData.IsPublished === false &&
+                                    entityType === 'Document'
+                                    ? false
+                                    : true;
+                            link.trashed = data.trashed;
+                            if (link.trashed) {
+                                item.url = vm.labels.general_recycleBin;
+                            }
+                        });
+                    } else {
+                        link.icon = 'icon-link';
+                        link.published = true;
+                    }
+                    currentForm.$setDirty();
+                }
+                editorService.close();
+            },
+            close: function () {
+                editorService.close();
+            }
+        };
+
+        editorService.open(linkpicker);
+    }
+    });
